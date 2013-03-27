59d3f2ccdb4adbcd9d1a793a3e05ee68461e2667
rswyatt1@gmail.com <rswyatt1@gmail.com>
2013-03-27 13:48:58 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BikeNav::Application.config.secret_token = '1bd222de0169fd795dbc3db20bc3bfc5424854abdbf1d445ceb8fd6ba110c2e0e5cfdc7260b80fd934745dfff0995abc73a79802b8cd43ae7d80243dfc016883'
